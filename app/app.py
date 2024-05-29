f3621cc047512e40882640226c12c37c574ad698
julienRactM <julien.ract@sfr.fr>
2024-05-29 16:10:25 +0200

    [Update] Sneakily deleted debug mode

--- app/app.py
+++ app/app.py
@@ -49,7 +49,7 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug = True)
+    app.run(host='0.0.0.0', port=5001)
     # app.run(host='0.0.0.0', port=8080)
     # print("app_started1")
     # app.run(debug=True)
